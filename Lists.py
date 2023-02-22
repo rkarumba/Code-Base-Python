a3f5654166354352e038d4a0ff57cd48cf598504
rkarumba <rkarumba@outlook.com>
2023-02-21 16:45:58 -0800

    Initial commit

---
+++ Lists.py
@@ -0,0 +1,49 @@
+Var = ["Geeks", "For", "Geeks"]
+print(Var)
+
+#Creating a List
+List = []
+print("Blank List: ")
+print(List)
+
+#Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers:")
+print(List)
+
+#Creating a List of Strings and accessing using index 
+List = ["Geeks", "For", "Geeks"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
+
+
+#Creating a List with duplicate Values 
+List = [1,2,4,4,3,3,3,6,5]
+print("\nList with the use of Numbers: ")
+print(List)
+
+#Creating a List with mixed Value types (Having Numbers and strings)
+List = [1,2,'Geeks',4,'For',6,'Geeks']
+print("\nList with the use of Mixed Values: ")
+print(List)
+
+# Accessing elements form a list 
+# Creating a List with multiple values 
+List = ["Geeks", "For", "Geeks"]
+
+
+# Accessing an element from the list by using an index number 
+print("Accessing an element from the list")
+print(List[0])
+print(List[2])
+
+#Accessing elements from a multi-dimensional list
+#
+# Creating a multi dimensional List by nesting a list inside of a list 
+List = [["Geeks", "for"], ["Geeks"]]
+
+#Accessing an element from a Multi-dimensional list 
+print("Accessing an element from a Multi-dimensional list")
+print(List[0][1])
+print(List[1][0])
